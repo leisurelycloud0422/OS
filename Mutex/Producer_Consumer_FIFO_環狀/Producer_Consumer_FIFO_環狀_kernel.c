fc1b18eb9b34aadd6d6dc57def45484d09279d52
leisurelycloud0422 <142489442+leisurelycloud0422@users.noreply.github.com>
2025-12-31 13:04:12 +0800

    Refactor Producer-Consumer kernel module code style

--- Mutex/Producer_Consumer_FIFO_環狀/Producer_Consumer_FIFO_環狀_kernel.c
+++ Mutex/Producer_Consumer_FIFO_環狀/Producer_Consumer_FIFO_環狀_kernel.c
@@ -9,8 +9,8 @@
 #define BUFFER_SIZE 5
 
 static int buffer[BUFFER_SIZE];
-static int front =0;
-static int rear  =0;
+static int front = 0;
+static int rear  = 0;
 
 static struct mutex lock;
 static wait_queue_head_t not_full;
@@ -22,30 +22,30 @@ static struct task_struct *consumer_thread;
 /* 判斷空 / 滿（與 user space 相同邏輯） */
 static bool is_empty(void)
 {
-return front == rear;
+    return front == rear;
 }
 
 static bool is_full(void)
 {
-return (rear +1) % BUFFER_SIZE == front;
+    return (rear + 1) % BUFFER_SIZE == front;
 }
 
 /* Producer thread */
 static int producer_fn(void *data)
 {
-int item =1;
+    int item = 1;
 
-while (!kthread_should_stop() && item <=10) {
+    while (!kthread_should_stop() && item <= 10) {
         mutex_lock(&lock);
 
-while (is_full()) {
+        while (is_full()) {
             mutex_unlock(&lock);
             wait_event(not_full, !is_full());
             mutex_lock(&lock);
         }
 
         buffer[rear] = item;
-        rear = (rear +1) % BUFFER_SIZE;
+        rear = (rear + 1) % BUFFER_SIZE;
 
         pr_info("[Producer] produced %d\n", item);
 
@@ -56,25 +56,25 @@ while (is_full()) {
         msleep(1000);
     }
 
-return 0;
+    return 0;
 }
 
 /* Consumer thread */
 static int consumer_fn(void *data)
 {
-int item;
+    int item;
 
-while (!kthread_should_stop()) {
+    while (!kthread_should_stop()) {
         mutex_lock(&lock);
 
-while (is_empty()) {
+        while (is_empty()) {
             mutex_unlock(&lock);
             wait_event(not_empty, !is_empty());
             mutex_lock(&lock);
         }
 
         item = buffer[front];
-        front = (front +1) % BUFFER_SIZE;
+        front = (front + 1) % BUFFER_SIZE;
 
         pr_info("[Consumer] consumed %d\n", item);
 
@@ -84,11 +84,11 @@ while (is_empty()) {
         msleep(2000);
     }
 
-return 0;
+    return 0;
 }
 
 /* module init */
-static int __initpc_init(void)
+static int __init pc_init(void)
 {
     pr_info("Producer-Consumer kernel module init\n");
 
@@ -96,19 +96,19 @@ static int __initpc_init(void)
     init_waitqueue_head(&not_full);
     init_waitqueue_head(&not_empty);
 
-    producer_thread = kthread_run(producer_fn,NULL,"producer_kthread");
-    consumer_thread = kthread_run(consumer_fn,NULL,"consumer_kthread");
+    producer_thread = kthread_run(producer_fn, NULL, "producer_kthread");
+    consumer_thread = kthread_run(consumer_fn, NULL, "consumer_kthread");
 
-return 0;
+    return 0;
 }
 
 /* module exit */
-static void __exitpc_exit(void)
+static void __exit pc_exit(void)
 {
-if (producer_thread)
+    if (producer_thread)
         kthread_stop(producer_thread);
 
-if (consumer_thread)
+    if (consumer_thread)
         kthread_stop(consumer_thread);
 
     pr_info("Producer-Consumer kernel module exit\n");
@@ -120,4 +120,5 @@ module_exit(pc_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("BMC learner");
 MODULE_DESCRIPTION("Producer Consumer using mutex + wait queue");
+
 
