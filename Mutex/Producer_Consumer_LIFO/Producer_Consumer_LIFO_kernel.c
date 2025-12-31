d4ef4153d897aaa4732d722891f6e371f1c6c3d6
leisurelycloud0422 <142489442+leisurelycloud0422@users.noreply.github.com>
2025-12-31 12:39:21 +0800

    Format code for consistency and readability

--- Mutex/Producer_Consumer_LIFO/Producer_Consumer_LIFO_kernel.c
+++ Mutex/Producer_Consumer_LIFO/Producer_Consumer_LIFO_kernel.c
@@ -9,8 +9,8 @@
 #define BUFFER_SIZE 5
 
 static int buffer[BUFFER_SIZE];
-static int front =0;
-static int rear  =0;
+static int front = 0;
+static int rear  = 0;
 
 static struct mutex lock;
 static wait_queue_head_t not_full;
@@ -22,27 +22,27 @@ static struct task_struct *consumer_task;
 /* 判斷是否空或滿 */
 static int is_empty(void)
 {
-return front == rear;
+    return front == rear;
 }
 
 static int is_full(void)
 {
-return (rear +1) % BUFFER_SIZE == front;
+    return (rear + 1) % BUFFER_SIZE == front;
 }
 
 /* Producer thread */
 static int producer_fn(void *data)
 {
-int i =1;
+    int i = 1;
 
-while (!kthread_should_stop() && i <=10) {
+    while (!kthread_should_stop() && i <= 10) {
         mutex_lock(&lock);
 
-/* buffer 滿就睡 */
+        /* buffer 滿就睡 */
         wait_event(not_full, !is_full());
 
         buffer[rear] = i;
-        rear = (rear +1) % BUFFER_SIZE;
+        rear = (rear + 1) % BUFFER_SIZE;
 
         pr_info("[Producer] Produced: %d\n", i);
         i++;
@@ -52,22 +52,22 @@ while (!kthread_should_stop() && i <=10) {
 
         msleep(1000);
     }
-return 0;
+    return 0;
 }
 
 /* Consumer thread */
 static int consumer_fn(void *data)
 {
-int item;
+    int item;
 
-while (!kthread_should_stop()) {
+    while (!kthread_should_stop()) {
         mutex_lock(&lock);
 
-/* buffer 空就睡 */
+        /* buffer 空就睡 */
         wait_event(not_empty, !is_empty());
 
         item = buffer[front];
-        front = (front +1) % BUFFER_SIZE;
+        front = (front + 1) % BUFFER_SIZE;
 
         pr_info("[Consumer] Consumed: %d\n", item);
 
@@ -76,7 +76,7 @@ while (!kthread_should_stop()) {
 
         msleep(2000);
     }
-return 0;
+    return 0;
 }
 
 /* Module init */
@@ -88,23 +88,23 @@ static int __init pc_init(void)
     init_waitqueue_head(&not_full);
     init_waitqueue_head(&not_empty);
 
-    producer_task = kthread_run(producer_fn,NULL,"pc_producer");
-    consumer_task = kthread_run(consumer_fn,NULL,"pc_consumer");
+    producer_task = kthread_run(producer_fn, NULL, "pc_producer");
+    consumer_task = kthread_run(consumer_fn, NULL, "pc_consumer");
 
-if (IS_ERR(producer_task) || IS_ERR(consumer_task)) {
+    if (IS_ERR(producer_task) || IS_ERR(consumer_task)) {
         pr_err("Failed to create kthreads\n");
-return -ENOMEM;
+        return -ENOMEM;
     }
 
-return 0;
+    return 0;
 }
 
 /* Module exit */
 static void __exit pc_exit(void)
 {
-if (producer_task)
+    if (producer_task)
         kthread_stop(producer_task);
-if (consumer_task)
+    if (consumer_task)
         kthread_stop(consumer_task);
 
     pr_info("Producer-Consumer kernel module unloaded\n");
